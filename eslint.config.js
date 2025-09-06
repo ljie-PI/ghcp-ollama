db94b91c8858e8e1522ce1af6e107c4724677476
ljie-PI <ljie@lab440.tech>
2025-09-06 12:27:48 +0800

    Add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default [
+  {
+    files: ['src/**/*.js', 'tests/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.es2021
+      }
+    },
+    rules: {
+      'no-console': 'off',
+      'comma-dangle': ['error', 'only-multiline'],
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'double'],
+      'semi': ['error', 'always'],
+      'no-unused-vars': 'warn',
+      'prefer-const': 'error',
+      'no-var': 'error'
+    }
+  },
+  js.configs.recommended,
+  {
+    ignores: [
+      'node_modules/',
+      'copilot/',
+      'dist/'
+    ]
+  }
+];
